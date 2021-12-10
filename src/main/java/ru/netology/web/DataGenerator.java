e07970eb2249a3360553f778521c57adc9fb7f0f
AAB-87 <andrej-abrekov@yandex.ru>
2021-12-10 20:19:01 +0400

    Added DataGenerator

---
+++ src/main/java/ru/netology/web/DataGenerator.java
@@ -0,0 +1,25 @@
+package ru.netology.web;
+
+import com.github.javafaker.Faker;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+class DataGenerator {
+    public static final Faker faker = new Faker(new Locale("ru"));
+
+    private static String generateCity() {
+        return faker.address().city();
+    }
+    public static String generateDate() {
+        return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    public static String generateName() {
+        return faker.name().fullName();
+    }
+    public static String generatePhoneNumber() {
+        return faker.phoneNumber().phoneNumber();
+    }
+
+}
